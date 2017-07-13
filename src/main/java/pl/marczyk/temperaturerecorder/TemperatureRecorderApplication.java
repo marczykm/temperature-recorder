932bec0d580d4e2095677c03c0107e803965a836
Marcin Marczyk <m.l.marczyk@gmail.com>
2017-07-13 17:27:28 +0200

    initial commit

---
+++ src/main/java/pl/marczyk/temperaturerecorder/TemperatureRecorderApplication.java
@@ -0,0 +1,12 @@
+package pl.marczyk.temperaturerecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureRecorderApplication.class, args);
+	}
+}
